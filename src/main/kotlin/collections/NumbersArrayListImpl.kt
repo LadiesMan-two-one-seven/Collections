2c6ae9f5d11c09e53ebd5fa8510664f872fb4ad8
LadiesMan-two-one-seven <asanagaevskiyi@list.ru>
2026-05-19 17:15:49 +0300

    ArrayList pt.1

---
+++ src/main/kotlin/collections/NumbersArrayListImpl.kt
@@ -0,0 +1,14 @@
+package collections
+
+class NumbersArrayListImpl : NumbersMutableList {
+    override val size: Int
+        get() = TODO("Not yet implemented")
+
+    override fun add(number: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun get(index: Int): Int {
+        TODO("Not yet implemented")
+    }
+}
